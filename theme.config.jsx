32c0b1a9deeb360fa249faeeb5cf1c3c147eefac
Malcolm453 <malcolm@vetro.co.za>
2023-05-30 13:08:12 +0200

    added vetro docs site

---
+++ theme.config.jsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default {
+    logo: <span><img src="https://res.cloudinary.com/droskhnig/image/upload/v1683112492/vetro_logo_d2esig_kadmui.webp" /></span>,
+    project: {
+      
+    },
+    // ...
+  }
