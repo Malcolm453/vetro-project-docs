50db7194196a0e0eb48d82b1c787d57d087db329
Malcolm453 <malcolm@vetro.co.za>
2023-07-18 11:31:45 +0200

    added documents

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+const withNextra = require('nextra')({
+    theme: 'nextra-theme-docs',
+    themeConfig: './theme.config.jsx',
+  })
+   
+  module.exports = withNextra({
+    image: {
+        domains: ['res.cloudinary.com'],
+    },
+  }
+)
+   
+  // If you have other Next.js configurations, you can pass them as the parameter:
+  // module.exports = withNextra({ /* other next.js config */ })
