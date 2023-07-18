90e4cd2be56f80a55288ec76fce140742cf9510d
Malcolm453 <malcolm@vetro.co.za>
2023-07-18 11:46:17 +0200

    resolved conflicts

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
 const withNextra = require('nextra')({
     theme: 'nextra-theme-docs',
     themeConfig: './theme.config.jsx',
-
-     
-      // image: {
-      //     domains: ['res.cloudinary.com'],
-      // },
-    
   })
    
-  module.exports = withNextra()
+  module.exports = withNextra({
+    image: {
+        domains: ['res.cloudinary.com'],
+    },
+  }
+)
    
   // If you have other Next.js configurations, you can pass them as the parameter:
   // module.exports = withNextra({ /* other next.js config */ })
